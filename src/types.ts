96cc92c5840d7c871dcbf2bfedfdda0ced89af12
hashimoto <weavehashimoto@gmail.com>
2026-09-01 18:08:30 +0900

    9/1

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+// 型の作成
+// 参考: https://nakamuuu.blog/typescript-type-check/
+
+// 型の定義 ボタン
+export type Operator = "+" | "-" | "×" | "÷" ;
+
+// 型の定義 計算
+export type CalculationOperator = "=";
+
+export type Number = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+
+export type Integer = Number;
+

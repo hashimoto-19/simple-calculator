3469f79cc4bb7e9e839d6956479111084afc8b28
hashimoto <shuji8519@gmail.com>
2026-09-03 00:20:27 +0900

    型の決め方

--- src/types.ts
+++ src/types.ts
@@ -2,12 +2,18 @@
 // 参考: https://nakamuuu.blog/typescript-type-check/
 
 // 型の定義 ボタン
-export type Operator = "+" | "-" | "×" | "÷" ;
+export type Operator = "+" | "-" | "×" | "÷";
 
 // 型の定義 計算
 export type CalculationOperator = "=";
 
-export type Number = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+export type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 
-export type Integer = Number;
-
+// 電卓の状態
+export type CalculatorState = {};
+//ボタンの見た目
+export type CalculatorButton = {};
+// 小数点
+export type CalculatorDecimalPoint = {};
+// AC
+export type CalculatorAC = {};

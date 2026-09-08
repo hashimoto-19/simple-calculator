604aa22cfc194251144dd6b14781ef923b90d14c
hashimoto <shuji8519@gmail.com>
2026-09-08 20:25:37 +0900

    Update Display.tsx

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,7 @@
 type DisplayProps = {
-    // 文字を表示させるためのものなので、string型でよい
-    // 計算するときはnumber型
-    value: string;
-}
+  // 文字を表示させるためのものなので、string型でよい
+  // 計算するときはnumber型
+  value: string;
+};
 
-export function Display ({ value }: DisplayProps) {
-
-}
-
+export function Display({ value }: DisplayProps) {}

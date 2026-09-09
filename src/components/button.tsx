46158a2a5ae855e1851e9cff00fe07e854d924f0
hashimoto <weavehashimoto@gmail.com>
2026-09-09 17:58:59 +0900

    9/9

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -10,5 +10,5 @@ export function Button({ label, className, onClick }: ButtonProps) {
 
 // ButtonProps の型定義は、実行時に表示するものではなく、TypeScript のチェック専用だからです。
 
-// export default function Button() {}　読み込み時は {} を使いません。名前は変更できます。
-// export function Button() {}　名前付きで export します。複数 export できます
+// export default function Button() {}読み込み時は {} を使いません。名前は変更できます。
+// export function Button() {}名前付きで export します。複数 export できます
